7799f297681c301b9d663f606f10a65af9728db9
Nesreddin <nesreddinvelizade2018@gmail.com>
2023-11-18 16:20:35 +0400

    deyisim

--- project2/project2/settings.py
+++ project2/project2/settings.py
@@ -134,41 +134,9 @@ USE_TZ = True
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
-if DEBUG:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / "staticfiles"
-    MEDIA_URL = '/uploads/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'project2/uploads/')
-
-    DEFAULT_FILE_STORAGE='django.core.files.storage.FileSystemStorage'
-
-else:
-    AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME=env('AWS_S3_REGION_NAME')
-
-
-    DEFAULT_FILE_STORAGE = 'project2.custom_storages.MediaStorage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    AWS_DEFAULT_ACL= 'public-read'
-    AWS_S3_OBJECT_PARAMETERS= {
-        'Expires' : 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl' : 'max-age=94608000',
-    }
-    STATICFILES_LOCATION= 'static'
-    AWS_LOCATION='static'
-    AWS_S3_CUSTOM_DOMAIN=f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    STATIC_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    STATIC_ROOT= STATIC_URL
-
-    MEDIA_LOCATION= 'uploads'
-    IMAGE_SETTING_LOCATION= MEDIA_LOCATION + '/image-settings'
-    DOCUMENT_LOCATION= MEDIA_LOCATION + '/documents'
-
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_LOCATION = 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
